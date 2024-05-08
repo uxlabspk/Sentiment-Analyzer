03228890cd7037af3e56215a37f618307b07c88b
Uxlabspk <muhammadnaveedcis@gmail.com>
2024-05-08 21:48:54 +0500

    adding env file and setting the comments index number

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, render_template
 from googleapiclient.discovery import build
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from dotenv import load_dotenv
+import os
+import re
+
+load_dotenv()
+api_KEY = os.getenv('API_KEY')
 
 app = Flask(__name__)
 
@@ -13,17 +19,17 @@ def index():
 def comments():
     error_string = ""
     if request.method == 'POST' and 'video_id' in request.form:
-        video_id = request.form['video_id']
-        api_key = "AIzaSyCbVl5WF2TXBwN6RppZQe7-Trao38R2rEs"
+        video_id = get_youtube_video_id(request.form['video_id'])
+        api_key = api_KEY
         youtube = build('youtube', 'v3', developerKey=api_key)
         comments = youtube.commentThreads().list(
             part = "snippet",
             videoId = video_id,
             maxResults = 100
         ).execute()
-        # Initilizatoin
+
         analyzer = SentimentIntensityAnalyzer()
-        # Perform sentiment analysis on each comment
+
         for comment in comments['items']:
             text = comment['snippet']['topLevelComment']['snippet']['textDisplay']
             sentiment_score = analyzer.polarity_scores(text)
@@ -36,6 +42,16 @@ def comments():
         return render_template('index.html', error_code = error_string)
 
     return "sdf"
+
+
+
+def get_youtube_video_id(url):
+    pattern = r"^https:\/\/(?:www\.|m\.)?youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/|)([^#\&\?]*).*"
+    match = re.match(pattern, url)
+    if match:
+        return match.group(1)
+    else:
+        return "Invalid url"
 
 
 
