924a3a1c996727d1201d11d2ac123278046d5b60
Uxlabspk <muhammadnaveedcis@gmail.com>
2024-05-09 15:42:51 +0500

    setting ui

--- app.py
+++ app.py
@@ -21,21 +21,35 @@ def comments():
     if request.method == 'POST' and 'video_id' in request.form:
         video_id = get_youtube_video_id(request.form['video_id'])
         api_key = api_KEY
+        
         youtube = build('youtube', 'v3', developerKey=api_key)
-        comments = youtube.commentThreads().list(
-            part = "snippet",
-            videoId = video_id,
-            maxResults = 100
-        ).execute()
+        
+        try:
+            comments = youtube.commentThreads().list(
+                part = "snippet",
+                videoId = video_id,
+                maxResults = 100
+            ).execute()
+        except:
+            print("Something went wrong")
+            return render_template('404.html')
 
         analyzer = SentimentIntensityAnalyzer()
+
+        positive_Average = 0
+        negative_Average = 0
 
         for comment in comments['items']:
             text = comment['snippet']['topLevelComment']['snippet']['textDisplay']
             sentiment_score = analyzer.polarity_scores(text)
             comment['sentiment'] = sentiment_score
+
             
-        return render_template('comments.html', comments = comments['items'])
+            positive_Average += comment['sentiment']['pos']
+            negative_Average += comment['sentiment']['neg']
+
+            
+        return render_template('comments.html', comments = comments['items'], avg_positive = round(positive_Average), avg_negative = round(negative_Average))
 
     else:
         error_string = "error occur"
@@ -50,8 +64,7 @@ def get_youtube_video_id(url):
     if match:
         return match.group(1)
     else:
-        return "Invalid url"
-
+        return -1
 
 
 
