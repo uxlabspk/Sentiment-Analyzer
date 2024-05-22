a5812e32f7304eec56e7e741fd6c45d86c429e69
Muhammad Naveed <85978999+uxlabspk@users.noreply.github.com>
2024-05-22 18:37:54 +0500

    updating code

--- app.py
+++ app.py
@@ -49,30 +49,39 @@ def fetchChannelVideos(video_id, api_key):
     youtube = build('youtube', 'v3', developerKey=api_key)
     
     # Call the channels.list method to retrieve information about the channel
-    channels_response = youtube.channels().list(
-        part='contentDetails',
-        forUsername=video_id
+    # channels_response = youtube.channels().list(
+    #     part='contentDetails',
+    #     forUsername=video_id
+    # ).execute()
+
+    channels_response = youtube.search().list(
+        part='snippet'
     ).execute()
+
+    print(channels_response)
     
     # Get the channel ID
-    channel_id = channels_response['items'][0]['id']
+    # channel_id = channels_response['etag']
+    # # channel_id = channels_response['items'][0]['id']
     
-    # Call the playlistItems.list method to retrieve the most recent videos from the channel
-    playlist_items_response = youtube.playlistItems().list(
-        part='snippet',
-        playlistId=channel_id,
-        maxResults=10
-    ).execute()
+    # # # Call the playlistItems.list method to retrieve the most recent videos from the channel
+    # playlist_items_response = youtube.playlistItems().list(
+    #     part='snippet',
+    #     playlistId=channel_id,
+    #     maxResults=10
+    # ).execute()
+
+    # print(playlist_items_response)
     
-    # Extract video information
-    videos = []
-    for item in playlist_items_response['items']:
-        videos.append({
-            'title': item['snippet']['title'],
-            'video_id': item['snippet']['resourceId']['videoId']
-        })
+    # # Extract video information
+    # videos = []
+    # for item in playlist_items_response['items']:
+    #     videos.append({
+    #         'title': item['snippet']['title'],
+    #         'video_id': item['snippet']['resourceId']['videoId']
+    #     })
     
-    return render_template('channelVideos.html', videos=videos)
+    return render_template('channelVideos.html', videos=channels_response)
     # return render_template('404.html')
 
 
